88ad7a5f4cd45272f479e4adb1255fcaaddb6887
Arij_Aouadi <arij.aouadi@esprit.tn>
2023-05-05 07:22:27 +0100

    client amortization

--- src/app/components/Client/payment-history-client/payment-history-client.component.ts
+++ src/app/components/Client/payment-history-client/payment-history-client.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MonthlyPayment } from 'src/app/models/MonthlyPayment';
 import { CreditService } from 'src/app/services/credit.service';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-payment-history-client',
@@ -12,6 +13,7 @@ export class PaymentHistoryClientComponent {
   idCredit!: number;
   monthlyPayments!: MonthlyPayment[];
   lateDays!: Number;
+  pdfUrl!: any;
   constructor(private router: Router, private _service: CreditService, private route: ActivatedRoute) {
   }
 
@@ -26,5 +28,14 @@ export class PaymentHistoryClientComponent {
       this.lateDays = res;
 
     });
+}
+exportPDF2(){
+  this._service.exportPDF(this.idCredit,).subscribe( (data: Blob) => {
+    const blob = new Blob([data], { type: 'application/pdf' });
+    const filename = 'amortization.pdf';
+    saveAs(blob, filename);
+  },
+  error => console.error(error)
+);
 }
 }
