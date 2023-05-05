c5ce60402ba63af251e6ad8bcc2797bb6453ebd2
Arij_Aouadi <arij.aouadi@esprit.tn>
2023-05-05 10:24:07 +0100

    visitor map

---
+++ src/app/components/Visitor/map-visitor/map-visitor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-visitor',
+  templateUrl: './map-visitor.component.html',
+  styleUrls: ['./map-visitor.component.css']
+})
+export class MapVisitorComponent {
+
+}
